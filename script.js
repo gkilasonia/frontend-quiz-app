46a007b33560ccae6871d1c3d60d7685404fd4bd
gkilasonia <dgiorjio@gmail.com>
2025-05-24 11:39:56 +0400

    some js done

--- script.js
+++ script.js
@@ -16,7 +16,10 @@ const questionCurrentNumber = document.getElementById(
 ); // Increase/decrease depending on questions array
 const questionText = document.getElementById("question-text"); // Change depending on questions array
 const progressBar = document.getElementById("progress-bar"); // Increase/decrease depending on correct answers
-const answerText = document.getElementById("answer-text"); // Change depending on answers array
+const answerTextA = document.getElementById("answer-text-a"); // Change depending on answers array
+const answerTextB = document.getElementById("answer-text-b"); // Change depending on answers array
+const answerTextC = document.getElementById("answer-text-c"); // Change depending on answers array
+const answerTextD = document.getElementById("answer-text-d"); // Change depending on answers array
 const submitButton = document.getElementById("submit-answer-button"); // Check answer and switch to next question
 const errorMessageContainer = document.getElementById(
   "error-message-container"
@@ -27,6 +30,11 @@ const playAgainButton = document.getElementById("play-again-btn"); //Restart the
 
 const correctIncorrectIcon = document.querySelectorAll(".correct-incorrect"); // Change visibility of img from visible to hidden and change icon (correct; inccorect)
 
+const htmlLogoUrl = "./assets/images/icon-html.svg";
+const cssLogoUrl = "./assets/images/icon-css.svg";
+const jsLogoUrl = "./assets/images/icon-js.svg";
+const accessLogoUrl = "./assets/images/icon-accessibility.svg";
+
 let myDatabaseData = null;
 
 async function fetchDataOnce() {
@@ -35,9 +43,41 @@ async function fetchDataOnce() {
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
+
     const data = await response.json();
     myDatabaseData = data;
-    console.log("Data fetched and stored:", myDatabaseData);
+
+    const mediaQuery = "(min-width: 1025px)";
+    const mediaQueryList = window.matchMedia(mediaQuery);
+
+    let qustionNumber = 1;
+    let question = myDatabaseData.quizzes[0].questions[0].question;
+    let answers = myDatabaseData.quizzes[0].questions[0].options;
+
+    console.log(
+      "Data fetched and stored:",
+      myDatabaseData.quizzes[0].questions[0]
+    );
+
+    htmlLogoContainer.addEventListener("click", () => {
+      openHtmlQuiz();
+    });
+
+    function openHtmlQuiz() {
+      logoContainer.style.visibility = "visible";
+      welcomeContainer.style.display = "none";
+      mainQuizContainer.style.display = mediaQueryList ? "grid" : "flex";
+      finalResultContainer.style.display = "none";
+      logoImage.src = htmlLogoUrl;
+      logoText.innerText = "HTML";
+      questionCurrentNumber.innerText = qustionNumber;
+      questionText.innerText = question;
+      answerTextA.innerText = answers[0];
+      answerTextB.innerText = answers[1];
+      answerTextC.innerText = answers[2];
+      answerTextD.innerText = answers[3];
+    }
+
     processData();
   } catch (error) {
     console.error("Error fetching data:", error);
@@ -53,3 +93,37 @@ function processData() {
 }
 
 fetchDataOnce();
+
+cssLogoContainer.addEventListener("click", () => {
+  openCssQuiz();
+});
+jsLogoContainer.addEventListener("click", () => {
+  openJsQuiz();
+});
+accessLogoContainer.addEventListener("click", () => {
+  openAccessQuiz();
+});
+
+function openCssQuiz() {
+  logoContainer.style.visibility = "visible";
+  welcomeContainer.style.display = "none";
+  mainQuizContainer.style.display = "flex";
+  logoImage.src = "./assets/images/icon-css.svg";
+  logoText.innerText = "CSS";
+}
+
+function openJsQuiz() {
+  logoContainer.style.visibility = "visible";
+  welcomeContainer.style.display = "none";
+  mainQuizContainer.style.display = "flex";
+  logoImage.src = "./assets/images/icon-js.svg";
+  logoText.innerText = "Javascript";
+}
+
+function openAccessQuiz() {
+  logoContainer.style.visibility = "visible";
+  welcomeContainer.style.display = "none";
+  mainQuizContainer.style.display = "flex";
+  logoImage.src = "./assets/images/icon-accessibility.svg";
+  logoText.innerText = "Accessibility";
+}
